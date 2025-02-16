0e2ba2fd647824ee8dfeb1f89f7193c41f9bdea2
Harshada42 <harshu4601@gmail.com>
2025-02-16 16:08:14 +0530

    Add files via upload

---
+++ Full Stack Intern/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
